4decc59446aa0445cd850d69937925d08cf7126e
matoruru <matoruru@shgt.jp>
2019-03-09 01:46:49 +0900

    add config file and script

---
+++ polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
+
+# Launch bar
+polybar main &
+
+echo "Bars launched..."
