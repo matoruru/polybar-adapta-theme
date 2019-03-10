242f7222e86920dfc3198df1e09702d40aad2b57
matoruru <matoruru@shgt.jp>
2019-03-10 16:55:01 +0900

    remove redundants

--- polybar/updates.sh
+++ polybar/updates.sh
@@ -5,8 +5,6 @@ if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
 fi
 
 if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
-# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-# if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
     updates_aur=0
 fi
 
