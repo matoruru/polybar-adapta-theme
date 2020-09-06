9f3829e68ae2b95dd2b9bc06482494bca796f8eb
matoruru <matoruru@shgt.jp>
2020-09-07 04:50:02 +0900

    Replace directly output from script with named pipe

--- polybar/checkupdates.sh
+++ polybar/checkupdates.sh
@@ -1,17 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+if [ ! -p "/tmp/.polybar-checkupdates-log" ]; then
+    mkfifo /tmp/.polybar-checkupdates-log
+fi
+
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l); then
     updates_arch=0
 fi
 
-if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+if ! updates_aur=$(yay -Qua 2> /dev/null | wc -l); then
     updates_aur=0
 fi
 
 updates=$(("$updates_arch" + "$updates_aur"))
 
 if [ "$updates" -gt 0 ]; then
-    echo " $updates"
+    result=" $updates"
 else
-    echo " $updates"
+    result=" $updates"
 fi
+
+echo "$result" > /tmp/.polybar-checkupdates-log
--- polybar/updates.sh
+++ polybar/updates.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 echo
-echo 'updates.sh: "pacman -Syu"'
-echo
-sudo pacman -Syu
-
-echo
 echo 'updates.sh: "yay -Syu"'
 echo
 yay -Syu
 
 echo
+bash ~/.config/polybar/checkupdates.sh
+
 read -p "Press enter to close this window..."
