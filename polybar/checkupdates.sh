dcc7047a6cfc3e8074819054dd2b3bd241112f37
matoruru <matoruru@shgt.jp>
2020-09-11 19:37:06 +0900

    Fix update script

--- polybar/checkupdates.sh
+++ polybar/checkupdates.sh
@@ -4,6 +4,8 @@ if [ ! -p "/tmp/.polybar-checkupdates-log" ]; then
     mkfifo /tmp/.polybar-checkupdates-log
 fi
 
+yay -Syy > /dev/null
+
 if ! updates=$(yay -Qu 2> /dev/null | wc -l); then
     updates=0
 fi
